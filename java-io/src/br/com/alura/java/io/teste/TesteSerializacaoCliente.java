62c76a75cee5cffa4433edaaa7040e7f9b7ce97a
Vanderlei Silva <vanderlei_nsilva@hotmail.com>
2020-02-16 21:18:17 -0300

    aula 7

---
+++ java-io/src/br/com/alura/java/io/teste/TesteSerializacaoCliente.java
@@ -0,0 +1,30 @@
+package br.com.alura.java.io.teste;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class TesteSerializacaoCliente {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		
+		/*Cliente cliente = new Cliente();
+		cliente.setNome("nico");
+		cliente.setProfissao("Dev");
+		cliente.setCpf("634724389");
+		
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("cliente.bin"));
+		oos.writeObject(cliente);
+		oos.close();*/
+		
+		
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("cliente.bin"));
+        Cliente cliente = (Cliente) ois.readObject();
+        ois.close();
+        System.out.println(cliente.getCpf());
+
+
+	}
+
+}
